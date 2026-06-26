b3746070543901d6e030874382a9135037ebd0a3
Jithin Joy Arakkal <jithinjoya12324@gmail.com>
2026-06-26 21:50:32 +0530

    Redesigned Onboard Page and Navbar

--- src/pages/Onboard.jsx
+++ src/pages/Onboard.jsx
@@ -4,7 +4,13 @@ function Onboard(){
     return(
         <div className="onboard-content">
             <h1>Welcome to <br /> Neighborhood Swap</h1>
-            <a href="/home"> <button className="start">Get Started</button> </a>
+            <div className="login-box">
+                <input type='text' placeholder='Email' className='login-input'/>
+                <input type='text' placeholder='Password' className='login-input'/>
+            </div>
+            <a href="/home"> <button className="login-button">Login</button> </a>
+            <p>or</p>
+            <a href="/home"> <button className="signup-button">Sign Up</button> </a>
         </div>
     );
 }
